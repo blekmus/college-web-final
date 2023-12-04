05cc94fc38a4ab30f9f326e13783b5c009c98197
dinil1 <29833601-dinil1@users.noreply.replit.com>
2023-12-04 21:44:34 +0000

    Initial commit

---
+++ scripts/footer.js
@@ -0,0 +1,46 @@
+// Function to generate and append the footer dynamically
+function generateFooter() {
+  // Create the main container div for the footer
+  var footerContainer = document.createElement('div');
+  footerContainer.classList.add('footer', 'container');
+
+  // Create the left div for the footer
+  var leftDiv = document.createElement('div');
+  leftDiv.classList.add('left');
+
+  // Create the image element for the footer
+  var image = document.createElement('img');
+  image.src = '../images/footer_dpad.png';
+
+  // Append the image to the left div
+  leftDiv.appendChild(image);
+
+  // Create the right div for the footer
+  var rightDiv = document.createElement('div');
+  rightDiv.classList.add('right');
+
+  // Create the paragraphs for the footer
+  var warningParagraph = document.createElement('p');
+  warningParagraph.textContent = 'WARNING: If you have epilepsy or have had seizures or other unusual reactions to flashing lights or patterns, consult a doctor before playing video games. All users should read the Health and Safety Information available in the system settings before using this software.';
+
+  // Create a line break element for better separation
+  var lineBreak = document.createElement('br');
+
+  var copyrightParagraph = document.createElement('p');
+  copyrightParagraph.textContent = '©Nintendo. Games are property of their respective owners.';
+
+  // Append paragraphs and line break to the right div
+  rightDiv.appendChild(warningParagraph);
+  rightDiv.appendChild(lineBreak);
+  rightDiv.appendChild(copyrightParagraph);
+
+  // Append left and right divs to the main container
+  footerContainer.appendChild(leftDiv);
+  footerContainer.appendChild(rightDiv);
+
+  // Append the main container to the bottom of the page
+  document.body.appendChild(footerContainer);
+}
+
+// Call the function to generate and append the footer
+generateFooter();
---
+++ scripts/header.js
@@ -0,0 +1,66 @@
+// Function to generate navigation bar dynamically
+function generateNavigation() {
+  // Create the main container div
+  var containerDiv = document.createElement("div");
+  containerDiv.classList.add("navigation", "container");
+
+  // Create left link with Nintendo logo
+  var leftLink = document.createElement("a");
+  leftLink.href = "./index.html";
+  leftLink.classList.add("left");
+
+  var img = document.createElement("img");
+  img.src = "../images/nintendo_logo.png";
+  img.alt = "nintendo_icon";
+  img.classList.add("icon");
+
+  leftLink.appendChild(img);
+  containerDiv.appendChild(leftLink);
+
+  // Create right div with navigation links
+  var rightDiv = document.createElement("div");
+  rightDiv.classList.add("right");
+
+  // Create "Buy Now" link
+  var buyNowLink = document.createElement("a");
+  buyNowLink.href = "./product.html";
+  // Add underline and red styling if on the product page
+  if (window.location.href.includes("product.html")) {
+    buyNowLink.classList.add("underline");
+  }
+  buyNowLink.classList.add("red");
+  buyNowLink.textContent = "Buy Now";
+
+  // Create "About" link
+  var aboutLink = document.createElement("a");
+  aboutLink.href = "./about.html";
+  // Add underline if on the about page
+  if (window.location.href.includes("about.html")) {
+    aboutLink.classList.add("underline");
+  }
+  aboutLink.textContent = "About";
+
+  // Create "Contact" link
+  var contactLink = document.createElement("a");
+  contactLink.href = "./contact.html";
+  // Add underline if on the contact page
+  if (window.location.href.includes("contact.html")) {
+    contactLink.classList.add("underline");
+  }
+  contactLink.textContent = "Contact";
+
+  // Append links to the right div
+  rightDiv.appendChild(buyNowLink);
+  rightDiv.appendChild(aboutLink);
+  rightDiv.appendChild(contactLink);
+
+  // Append the right div to the main container div
+  containerDiv.appendChild(rightDiv);
+
+  // Append the generated HTML to the body or any other container element
+  const body = document.body;
+  body.insertBefore(containerDiv, body.firstChild);
+}
+
+// Call the function to generate the HTML navigation bar
+generateNavigation();
